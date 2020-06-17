3f66266a0f8fe7a171834bb31ae0cc06d860eae4
wzipone <wzipone76@gmail.com>
2020-06-17 17:53:45 +0800

    deploy

---
+++ precache-manifest.7fccef62095bbcc09461d18941ecb74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332295516adb6cd12321",
+    "url": "/money-vue-website-1/css/app.c942db2b.css"
+  },
+  {
+    "revision": "9c8376d2d7afa75c9f9888fc2be97e45",
+    "url": "/money-vue-website-1/index.html"
+  },
+  {
+    "revision": "332295516adb6cd12321",
+    "url": "/money-vue-website-1/js/app.86787b5b.js"
+  },
+  {
+    "revision": "3fea3669a1ae9b96bc27",
+    "url": "/money-vue-website-1/js/chunk-vendors.c409f931.js"
+  },
+  {
+    "revision": "40b1d1afffcbfad4ddfe5beeee9d7d15",
+    "url": "/money-vue-website-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue-website-1/robots.txt"
+  }
+]);
